16e6a93a7bcbf8e161aa82edb94f27935c7c98e8
07001deepak <113614991+07001deepak@users.noreply.github.com>
2023-10-13 21:43:21 +0530

    1. CollatzConjecture

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -13,31 +13,61 @@ import java.util.Scanner;
 public class CollatzConjecture{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n value");
-        int n = sc.nextInt();
-        while(n>0){
-            if(n==1){
-                System.out.print(n);
+        System.out.println("Enter n1 value");
+        int n1 = sc.nextInt();
+        int temp1 = n1;
+        System.out.println("Enter n2 value");
+        int n2 = sc.nextInt();
+        int temp2 = n2;
+        int c1=0;
+        int c2=0;
+        while(n1>0){
+            if(n1==1){
+                System.out.print(n1);
                 break;
             }
-            else if(n%2==0){
-                 System.out.print(n + ",");
-                  n = n/2;
+            else if(n1%2==0){
+                 System.out.print(n1 + ",");
+                  n1 = n1/2;
             }
             else{
-                System.out.print(n + ",");
-                n = (3 * n)+1;
+                System.out.print(n1 + ",");
+                n1 = (3 * n1)+1;
             }
+            c1++;
         }
-
+        System.out.println();
+        while(n2>0){
+            if(n2==1){
+                System.out.print(n2);
+                break;
+            }
+            else if(n2%2==0){
+                 System.out.print(n2 + ",");
+                  n2 = n2/2;
+            }
+            else{
+                System.out.print(n2 + ",");
+                n2 = (3 * n2)+1;
+            }
+            c2++;
+        }
+        System.out.println();
+        if(c1<c2)
+        System.out.println("Input" + " " + temp1 + " " + "has less number of steps to reach 1");
+        else
+        System.out.println("Input" + " " + temp2 + " " + "has less number of steps to reach 1");
     }
 }
 
-// Output:
-// Enter n value
-// 10
-// 10,5,16,8,4,2,1
+// Output
 
-// Enter n value
-// 8
+// Enter n1 value
+// 8 
+// Enter n2 value
+// 9 
 // 8,4,2,1
+// 9,28,14,7,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1
+// Input 8 has less number of steps to reach 1
+
+
